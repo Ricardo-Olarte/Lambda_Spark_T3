e3934833ed67c44abc6853f12ae4fdf86e6192f3
Ricardo Olarte <olarte.jose.r@gmail.com>
2023-09-06 13:03:46 -0500

    Agregando fases T1 y T2

--- src/main/java/edu/escuelaing/app/HttpServer.java
+++ src/main/java/edu/escuelaing/app/HttpServer.java
@@ -65,12 +65,9 @@ public class HttpServer {
                     break;
                 }
             }
-            System.out.println("URI: " + uriString);
-            if(uriString.startsWith("/hello!")){
+            //System.out.println("URI: " + uriString);
+            if(uriString.startsWith("/?t=")){
                 outputLine = getHello(uriString);
-            }else if (uriString.contains("/Movies")) {
-                String sufix = uriString.split("/")[2];
-                out.println(handlers.get("/Movies").handle("/" + sufix, null, null));
             }else{
                 outputLine = indexResponse();
             }
@@ -84,31 +81,75 @@ public class HttpServer {
     }
 
     /**
-     *
+     * Get information about the movie with a JSON string
      * @param uri
-     * @return
+     * @return String uri
      */
     public static String getHello(String uri){
-        String name = uri.replace("/hello?name=", "");
-        return "{ \"msg\": \"Hello " + name + "\"}";
+        HttpConnectionExample movie = new HttpConnectionExample();
+        String htmlFirst = "HTTP/1.1 200 OK\r\n" + "Content-type: text/html\r\n" + "\r\n" + "<!DOCTYPE html>\n" + "<html>\n" + "<div>\n" + "<p>\n";
+        String htmlSecond = "</p>\n" + "</div>\n" + "</html>";
+        try {
+            String json = movie.service(uri).replace("{", "").replace("}", "");
+            String[] infoMovie = json.split(",");
+            String htmlBody = "";
+            //System.out.println(uri);
+            //System.out.println(movie.service(uri));
+            //System.out.println(json);
+            for(String keyInfo : infoMovie){
+                htmlBody += keyInfo.replace("\"","") + "<br/>";
+            }
+            //System.out.println(htmlBody);
+            return htmlFirst + htmlBody + htmlSecond;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
+     * It's HTML string, use to view for the user
      * @return
      */
     public static  String indexResponse(){
-        byte[] encodedBytes = new byte[0];
-        try{
-            encodedBytes = Files.readAllBytes(Paths.get("src/main/resources/index.html"));
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        String indexHTML = new String(encodedBytes, StandardCharsets.UTF_8);
         String response = "HTTP/1.1 200 OK\r\n"
                 + "Content-type: text/html\r\n"
                 + "\r\n"
                 + "<!DOCTYPE html>\n"
-                + indexHTML;
+                + "<html>\n"
+                + "    <head>\n"
+                + "        <title>T1</title>\n"
+                + "        <meta charset=\"UTF-8\">\n"
+                + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "    </head>\n"
+                + "    <style>\n"
+                + "    body {background-color: black;color: white;display:flex;justify-content:center;flex-direction:column;align-items:center}"
+                + "    .input-button{"
+                + "    background-color: white;padding:4px 6px;border-radius:8px}"
+                + "    #getrespmsg{max-width:768px;margin:0 auto}"
+                + "    </style>\n"
+                + "    <body>\n"
+                + "        <h1><center>Name of the movie</center></h1>\n"
+                + "        <form action=\"/hello\">\n"
+                + "            <label for=\"name\">Name:</label><br>\n"
+                + "            <input type=\"text\" id=\"name\" name=\"name\" value=\"\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\" class=\"input-button\">\n"
+                + "        </form> \n"
+                + "        <div id=\"getrespmsg\"></div>\n"
+                + "\n"
+                + "        <script>\n"
+                + "            function loadGetMsg() {\n"
+                + "                let nameVar = document.getElementById(\"name\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                }\n" + "                xhttp.open(\"GET\", \"/?t=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "        </script>\n"
+                + "\n"
+                + "    </body>\n"
+                + "</html>";
         return response;
     }
 
